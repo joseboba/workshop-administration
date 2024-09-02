7a82bc052397ba87ebc9755218c92135e8754c5c
Jose Bobadilla <jbobadilla@is4tech.com>
2024-09-01 21:55:48 -0600

    return whitelist a false

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.setGlobalPrefix('workshop-administration');
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
+      whitelist: false,
       forbidNonWhitelisted: false,
     }),
   );
